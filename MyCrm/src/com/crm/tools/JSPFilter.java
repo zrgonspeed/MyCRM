3f2f0a8aee729232ba0b98115d3f69d7f69fd8a1
zhangriguang <zhangriguang@chuhui.com>
2020-12-28 17:53:04 +0800

    jsp拦截，除了login.jsp外，其他jsp不能直接访问。检查登录过滤器，不拦静态资源，貌似拦了action,跟struts拦截器重复。

---
+++ MyCrm/src/com/crm/tools/JSPFilter.java
@@ -0,0 +1,53 @@
+package com.crm.tools;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.crm.info.HrEmployee;
+
+public class JSPFilter implements Filter {
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		
+		String path = req.getRequestURI();
+        
+		// 如果包含login.jsp证明是登陆就放行
+		if (path.contains("login") || 
+				path.contains("/js/") || 
+				path.contains("/image/") || 
+				path.contains("/media/")  ||
+				path.contains("/img/") || 
+				path.contains("/css/")
+				 || path.contains("/images/")) {
+			chain.doFilter(request, response); // 放行
+			return;
+		}
+		
+		HttpSession session = req.getSession();
+		HrEmployee emp = (HrEmployee) session.getAttribute("emp");
+		// 如果session中存在emp证明用户登录，可以放行。否则认为未登陆重定向到login.jsp
+		if (emp == null) {
+			res.sendRedirect(req.getContextPath() + "/login.jsp");
+		} else {
+			chain.doFilter(request, response);
+		}
+
+	}
+
+	public void destroy() {
+	}
+
+	public void init(FilterConfig arg0) throws ServletException {
+	}
+}
